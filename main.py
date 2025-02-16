b17e9e3285f4de2ad206ae97aeb345e58306d711
Emile Aydar <114087019+EmileAydar@users.noreply.github.com>
2025-02-16 19:03:36 +0100

    main script

--- main.py
+++ main.py
@@ -1,36 +1,33 @@
-from flask import Flask, jsonify, render_template, send_from_directory
+from flask import Flask, jsonify, render_template
 from flask_cors import CORS
 import numpy as np
-from vpython import *
-app = Flask(__name__)
+
+app = Flask(__name__, template_folder='C:\\Users\\yunus\\Desktop\\dyson_sphere', static_folder='static') # user needs to change path
 CORS(app)
 
 @app.route('/model', methods=['GET'])
-def index():
-    vertices,faces = create_dyson_sphere()
-    return jsonify({
-        'vertices': vertices,
-        'faces': faces })
+def model():
+    # Increase subdivisions for a smoother sphere (adjust radius as desired)
+    vertices, faces = create_dyson_sphere(radius=10.0, subdivisions=3)
+    return jsonify({'vertices': vertices, 'faces': faces})
 
-def create_dyson_sphere(radius=1.0, subdivisions=2):
+def create_dyson_sphere(radius=10.0, subdivisions=3):
     def icosphere(radius, subdivisions):
         t = (1.0 + np.sqrt(5.0)) / 2.0
 
         vertices = np.array([
-            [-1, t, 0],
-            [1, t, 0],
-            [-1, -t, 0],
-            [1, -t, 0],
-
-            [0, -1, t],
-            [0, 1, t],
-            [0, -1, -t],
-            [0, 1, -t],
-
-            [t, 0, -1],
-            [t, 0, 1],
-            [-t, 0, -1],
-            [-t, 0, 1],
+            [-1,  t,  0],
+            [ 1,  t,  0],
+            [-1, -t,  0],
+            [ 1, -t,  0],
+            [ 0, -1,  t],
+            [ 0,  1,  t],
+            [ 0, -1, -t],
+            [ 0,  1, -t],
+            [ t,  0, -1],
+            [ t,  0,  1],
+            [-t,  0, -1],
+            [-t,  0,  1],
         ])
 
         faces = np.array([
@@ -39,67 +36,66 @@ def create_dyson_sphere(radius=1.0, subdivisions=2):
             [0, 1, 7],
             [0, 7, 10],
             [0, 10, 11],
-
             [1, 5, 9],
             [5, 11, 4],
             [11, 10, 2],
             [10, 7, 6],
             [7, 1, 8],
-
             [3, 9, 4],
             [3, 4, 2],
             [3, 2, 6],
             [3, 6, 8],
             [3, 8, 9],
-
             [4, 9, 5],
             [2, 4, 11],
             [6, 2, 10],
             [8, 6, 7],
-            [9, 8, 1],])
+            [9, 8, 1],
+        ])
+
+        # Normalize the initial vertices
+        vertices = np.array([v/np.linalg.norm(v) for v in vertices])
 
         for _ in range(subdivisions):
+            midpoint_cache = {}
             new_faces = []
+            vertices_list = list(vertices)
 
-            for face in faces:
-                v1, v2, v3 = vertices[face]
+            def get_midpoint(i, j):
+                key = tuple(sorted((i, j)))
+                if key in midpoint_cache:
+                    return midpoint_cache[key]
+                # Compute and normalize the midpoint
+                v = (vertices[i] + vertices[j]) / 2
+                v = v / np.linalg.norm(v)
+                vertices_list.append(v)
+                index = len(vertices_list) - 1
+                midpoint_cache[key] = index
+                return index
 
-                v12 = np.add(v1, v2) / 2
-                v23 = np.add(v2, v3) / 2
-                v31 = np.add(v3, v1) / 2
-
-                v12 /= np.linalg.norm(v12)
-                v23 /= np.linalg.norm(v23)
-                v31 /= np.linalg.norm(v31)
-
-
-
-                v12_idx = len(vertices)
-                v23_idx = v12_idx + 1
-                v31_idx = v12_idx + 2
-
-                vertices = np.vstack([vertices, v12, v23, v31])
-
+            for tri in faces:
+                i0, i1, i2 = tri
+                a = get_midpoint(i0, i1)
+                b = get_midpoint(i1, i2)
+                c = get_midpoint(i2, i0)
                 new_faces.extend([
-                    [face[0], v12_idx, v31_idx],
-                    [v12_idx, face[1], v23_idx],
-                    [v31_idx, v23_idx, face[2]],
-                    [v12_idx, v23_idx, v31_idx],])
-
+                    [i0, a, c],
+                    [i1, b, a],
+                    [i2, c, b],
+                    [a, b, c]
+                ])
+            vertices = np.array(vertices_list)
             faces = np.array(new_faces)
 
         vertices *= radius
-
         return vertices, faces
 
     vertices, faces = icosphere(radius, subdivisions)
     return vertices.tolist(), faces.tolist()
 
-
 @app.route('/')
-def serve_index():
-    ##change the pathway when you copy the code
-    return send_from_directory('C:\\Users\\yunus\\PycharmProjects\\DysonSphere\\templates','index.html')
+def index():
+    return render_template('index.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
-
